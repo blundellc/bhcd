2d2ac12f913096b39aacfd907e01c641ab39d29b
charles blundell <c.blundell@gatsby.ucl.ac.uk>
2013-03-23 14:13:33 +0000

    fastnrt command line options

--- src/fastnrt.c
+++ src/fastnrt.c
@@ -1,9 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <glib.h>
+#include <glib/gprintf.h>
 #include "nrt.h"
 
-void run(GRand * rng, Dataset * dataset, gboolean verbose) {
+
+static gboolean binary_only = FALSE;
+static guint build_restarts = 1000;
+static guint seed = 0x2a23b6bb;
+static gdouble param_gamma = 0.4;
+static gdouble param_alpha = 1.0;
+static gdouble param_beta = 0.2;
+static gdouble param_delta = 0.2;
+static gdouble param_lambda = 1.0;
+static gchar *	test_fname = NULL;
+static gchar 	output_prefix_default[] = "output/out";
+static gchar *	output_prefix = output_prefix_default;
+static gchar *	output_tree_fname = NULL;
+static gchar *	output_pred_fname = NULL;
+static gchar *	output_time_fname = NULL;
+
+static GOptionEntry options[] = {
+	{ "seed",	 's', 0, G_OPTION_ARG_INT,	&seed,		"set RNG seed to S",		"S" },
+
+	{ "binary-only", 'B', 0, G_OPTION_ARG_NONE,	&binary_only, 	"only construct binary trees",	NULL },
+	{ "restarts",	 'R', 0, G_OPTION_ARG_INT,	&build_restarts,"take best of N restarts",	"N" },
+
+	{ "test-file",	 't', 0, G_OPTION_ARG_FILENAME,	&test_fname,	"test dataset", NULL },
+	{ "prefix",	 'p',  0, G_OPTION_ARG_STRING,	&output_prefix,	"prefix for output filenames", NULL },
+
+	{ "gamma",	 'g', 0, G_OPTION_ARG_DOUBLE,	&param_gamma,	"set mixture parameter to GAMMA","GAMMA" },
+	{ "alpha",	 'a', 0, G_OPTION_ARG_DOUBLE,	&param_alpha,	"set on-diagonal one hyperparameter to ALPHA", "ALPHA" },
+	{ "beta",	 'b', 0, G_OPTION_ARG_DOUBLE,	&param_beta,	"set on-diadonal zero hyperparameter to BETA", "BETA" },
+	{ "delta",	 'd', 0, G_OPTION_ARG_DOUBLE,	&param_delta,	"set off-diagonal one hyperparameter to DELTA", "DELTA" },
+	{ "lambda",	 'l', 0, G_OPTION_ARG_DOUBLE,	&param_lambda,	"set off-diadonal zero hyperparameter to LAMBDA", "LAMBDA" },
+	{ NULL,		   0, 0, 0,			NULL,		NULL, NULL },
+};
+
+gchar * parse_args(int *argc, char ***argv) {
+	GError * error;
+	GOptionContext * ctx;
+
+	ctx = g_option_context_new("FILE -- fit an NRT");
+	g_option_context_add_main_entries(ctx, options, NULL);
+	error = NULL;
+	if (!g_option_context_parse(ctx, argc, argv, &error)) {
+		g_print("option parsing failed: %s\n", error->message);
+		exit(1);
+	}
+	if (*argc < 2) {
+		g_print("missing argument FILE\n");
+		g_print("%s", g_option_context_get_help(ctx, TRUE, NULL));
+		exit(1);
+	}
+	if (*argc > 2) {
+		g_print("too many arguments\n");
+		g_print("%s", g_option_context_get_help(ctx, TRUE, NULL));
+		exit(1);
+	}
+	output_tree_fname = g_strdup_printf("%s.tree", output_prefix);
+	output_pred_fname = g_strdup_printf("%s.pred", output_prefix);
+	output_time_fname = g_strdup_printf("%s.time", output_prefix);
+	return (*argv)[1];
+}
+
+Tree * run(GRand * rng, Dataset * dataset, gboolean verbose) {
 	Params * params;
 	Tree * root;
 
@@ -11,17 +73,21 @@ void run(GRand * rng, Dataset * dataset, gboolean verbose) {
 		dataset_println(dataset, "");
 	}
 
-	params = params_default(dataset);
+	params = params_new(dataset,
+			param_gamma,
+			param_alpha, param_beta,
+			param_delta, param_lambda);
+	params->binary_only = binary_only;
 
-	root = build_repeat(rng, params, 1000);
-
-	tree_println(root, "result: ");
-
-	tree_io_save(root, "output/out.tree");
+	root = build_repeat(rng, params, build_restarts);
 
 	g_assert(tree_num_leaves(root) == dataset_num_labels(dataset));
-	tree_unref(root);
 	params_unref(params);
+	return root;
+}
+
+void timer_save_io(GTimer * timer, GIOChannel * io) {
+	io_printf(io, "time: %es\n", g_timer_elapsed(timer, NULL));
 }
 
 int main(int argc, char * argv[]) {
@@ -29,18 +95,29 @@ int main(int argc, char * argv[]) {
 	GTimer * timer;
 	Dataset * dataset;
 	gchar *fname;
+	Tree * root;
 
-	fname = argv[1];
+	fname = parse_args(&argc, &argv);
 
-	rng = g_rand_new_with_seed(0x2a23b6bb);
+	g_print("seed: %u\n", seed);
+	rng = g_rand_new_with_seed(seed);
 	timer = g_timer_new();
 	dataset = dataset_gml_load(fname);
 
 	g_timer_start(timer);
-	run(rng, dataset, FALSE);
+	root = run(rng, dataset, FALSE);
 	g_timer_stop(timer);
-	g_print("time: %es\n", g_timer_elapsed(timer, NULL));
 
+	io_stdout((IOFunc)timer_save_io, timer);
+	tree_println(root, "tree: ");
+
+	io_file(output_time_fname, (IOFunc)timer_save_io, timer);
+	tree_io_save(root, output_tree_fname);
+
+	tree_unref(root);
+	g_free(output_tree_fname);
+	g_free(output_pred_fname);
+	g_free(output_time_fname);
 	dataset_unref(dataset);
 	g_timer_destroy(timer);
 	g_rand_free(rng);
