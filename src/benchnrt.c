e9557b2744e1b2a30ff71dc4302679be59a733ab
charles blundell <c.blundell@gatsby.ucl.ac.uk>
2013-03-24 21:24:17 +0000

    initial parallelised benchmark nrt

--- src/benchnrt.c
+++ src/benchnrt.c
@@ -3,73 +3,142 @@
 #include <glib.h>
 #include "nrt.h"
 
-Tree * run_rand(GRand * rng, guint num_items, gdouble sparsity, guint verbose) {
+
+typedef struct job {
+	guint num_items;
+	gdouble sparsity;
+} Job;
+
+typedef struct result {
+	Job * job;
+	Tree * best_tree;
+	gdouble worst_time;
+} Result;
+
+static void job(Job * job, GAsyncQueue * result_queue);
+static void result_free(gpointer pres);
+static Tree * run_rand(guint num_items, gdouble sparsity);
+
+
+static int small_jobs_first(Job *aa, Job *bb, gpointer junk) {
+	return aa->num_items - bb->num_items;
+}
+
+
+static void result_free(gpointer pres) {
+	Result * res = pres;
+	g_free(res->job);
+	tree_unref(res->best_tree);
+	g_free(res);
+}
+
+static void job(Job * job, GAsyncQueue * result_queue) {
+	Result * result;
+	GTimer *timer;
+	Tree * tree;
+
+	timer = g_timer_new();
+	result = NULL;
+
+	g_print("job %d start\n", job->num_items);
+	for (guint repeat = 0; repeat < 100; repeat++) {
+		g_timer_start(timer);
+		tree = run_rand(job->num_items, job->sparsity);
+		g_timer_stop(timer);
+		if (result == NULL) {
+			result = g_new(Result, 1);
+			result->job = job;
+			result->best_tree = tree;
+			result->worst_time = g_timer_elapsed(timer, NULL);
+		} else if (g_timer_elapsed(timer, NULL) > result->worst_time) {
+			result->worst_time = g_timer_elapsed(timer, NULL);
+		} else if (tree_get_logprob(tree) > tree_get_logprob(result->best_tree)) {
+			tree_unref(result->best_tree);
+			result->best_tree = tree;
+		} else {
+			tree_unref(tree);
+		}
+	}
+	g_async_queue_push(result_queue, result);
+	g_print("job %d done!\n", job->num_items);
+}
+
+
+static Tree * run_rand(guint num_items, gdouble sparsity) {
 	Dataset * dataset;
 	Params * params;
 	Tree * root;
 	Build * build;
+	GRand * rng;
 
+	rng = g_rand_new();
 	/* dataset = dataset_gen_speckle(rng, num_items, 1.0-sparsity); */
-	dataset = dataset_gen_blocks(rng, num_items, 3, sparsity);
-	if (verbose > 1) {
-		dataset_println(dataset, "");
-	}
+	dataset = dataset_gen_blocks(rng, num_items, 5, sparsity);
 
 	params = params_default(dataset);
 	dataset_unref(dataset);
 
 	build = build_new(rng, params, 200, FALSE);
+	build_set_verbose(build, FALSE);
 	build_run(build);
 	root = build_get_best_tree(build);
 	tree_ref(root);
 	build_free(build);
 
-	if (verbose) {
-		tree_println(root, "result: ");
-	}
-
 	g_assert(tree_num_leaves(root) == dataset_num_labels(dataset));
 	params_unref(params);
+	g_rand_free(rng);
 	return root;
 }
 
 int main(int argc, char * argv[]) {
-	GRand * rng;
 	guint num_items;
-	guint repeat;
 	gdouble sparsity;
-	gdouble max_time;
-	GTimer *timer;
-	Tree * root;
-	Tree * best;
+	GAsyncQueue * result_queue;
+	GThreadPool * pool;
+	const guint num_cores = 8;
+	GError * error;
+	gint pending_jobs;
 
-	rng = g_rand_new();
-	timer = g_timer_new();
-
-	sparsity = 0.2;
-	for (num_items = 10; num_items < 1000; num_items += 2) {
-		max_time = 0.0;
-		best = NULL;
-		for (repeat = 0; repeat < 100; repeat++) {
-			g_timer_start(timer);
-			root = run_rand(rng, num_items, sparsity, 0);
-			g_timer_stop(timer);
-			if (g_timer_elapsed(timer, NULL) > max_time) {
-				max_time = g_timer_elapsed(timer, NULL);
-			}
-			if (best == NULL || tree_get_logprob(root) > tree_get_logprob(best)) {
-				tree_unref(best);
-				best = root;
-			} else {
-				tree_unref(root);
-			}
-		}
-		g_print("%d %2.2e ", num_items, max_time);
-		tree_println(best, "");
-		tree_unref(best);
+	error = NULL;
+	g_thread_init(NULL);
+	result_queue = g_async_queue_new_full(result_free);
+	pool = g_thread_pool_new((GFunc)job, result_queue, num_cores, TRUE, &error);
+	if (error != NULL) {
+		g_error("g_thread_pool_new: %s", error->message);
 	}
-	g_timer_destroy(timer);
-	g_rand_free(rng);
+	g_thread_pool_set_sort_function(pool, (GCompareDataFunc)small_jobs_first, NULL);
+	sparsity = 0.2;
+	pending_jobs = 0;
+	for (num_items = 2; num_items < 5; num_items += 2) {
+		Job * job = g_new(Job, 1);
+		job->num_items = num_items;
+		job->sparsity = sparsity;
+		g_thread_pool_push(pool, job, &error);
+		if (error != NULL) {
+			g_error("g_thread_pool_push: %s", error->message);
+		}
+		pending_jobs++;
+	}
+
+	while (pending_jobs > 0) {
+		// wait 30s between attempts to pop from queue (timed_pop for
+		// compat).
+		GTimeVal timeout;
+		g_get_current_time(&timeout);
+		g_time_val_add(&timeout, 30000000);
+		Result * result = g_async_queue_timed_pop(result_queue, &timeout);
+		while (result != NULL) {
+			g_print("%d %2.2e ", result->job->num_items, result->worst_time);
+			tree_println(result->best_tree, "");
+			result_free(result);
+			pending_jobs--;
+			result = g_async_queue_try_pop(result_queue);
+		}
+		g_print("status: %d pending\n", pending_jobs);
+	}
+	g_thread_pool_free(pool, TRUE, FALSE);
+	g_async_queue_unref(result_queue);
 	return 0;
 }
 
