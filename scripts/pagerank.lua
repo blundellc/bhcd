82742a853b956589d7abf1946d9f1129a414f3b7
charles blundell <c.blundell@gatsby.ucl.ac.uk>
2013-04-06 02:14:12 +0100

    simple lua implementation of pagerank and rooted pagerank (both untested)

---
+++ scripts/pagerank.lua
@@ -0,0 +1,192 @@
+#!/usr/bin/lua
+
+require 'nrt'
+
+AdjMtx = {}
+function AdjMtx:new(names)
+    local obj = {}
+    obj.names = names
+    table.sort(obj.names)
+    obj.num_elems = table.getn(names)
+    obj._incoming = {}
+    obj.num_outgoing = {}
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+function AdjMtx:vertices()
+    local ii = 0
+    local last = self.num_elems
+    local names = self.names
+    return function ()
+            ii = ii + 1
+            if ii <= last then
+                return names[ii]
+            end
+        end
+end
+
+function AdjMtx:add(src, dst)
+    if src == dst then
+        return
+    end
+    local incoming = self._incoming[dst]
+    if not incoming then
+        incoming = {}
+        self._incoming[dst] = incoming
+    end
+    if not incoming[src] then
+        incoming[src] = true
+        local num_out = self.num_outgoing[src] or 0
+        self.num_outgoing[src] = num_out + 1
+    end
+end
+
+function AdjMtx:incoming(uu)
+    local incoming = self._incoming[uu]
+    if incoming == nil then
+        return function () return; end
+    end
+    local vv = nil
+    return function ()
+            vv = next(incoming, vv)
+            if vv ~= nil then
+                return vv
+            end
+        end
+end
+
+function AdjMtx:print()
+    local max_len = string.len('src/dst')
+    for uu in self:vertices() do
+        local len = string.len(uu)
+        if len > max_len then
+            max_len = len
+        end
+    end
+    local fmt = string.format('%%%us', max_len)
+    local function atom(vv)
+            io.write(string.format(fmt, vv), ' ')
+        end
+
+    atom('src/dst')
+    for uu in self:vertices() do
+        atom(uu)
+    end
+    io.write('\n')
+    for src in self:vertices() do
+        atom(src)
+        for dst in self:vertices() do
+            if self._incoming[dst] then
+                atom(tostring(not not self._incoming[dst][src]))
+            else
+                atom(tostring(false))
+            end
+        end
+        io.write('\n')
+    end
+    io.write('\n')
+
+    for dst in self:vertices() do
+        atom(dst)
+        io.write(string.format('%2u ', self.num_outgoing[dst] or 0), ' ')
+        for src in self:incoming(dst) do
+            atom(src)
+        end
+        io.write('\n')
+    end
+    io.write('\n')
+end
+
+function load_train(fname)
+    local dd = dataset.load(fname)
+    print(dd)
+    local adj = AdjMtx:new(dd:labels())
+    for _, elem in pairs(dd:elems()) do
+        local src, dst = unpack(elem)
+        if dd:get(src, dst) then
+            adj:add(src, dst)
+        end
+    end
+    return adj
+end
+
+function pagerank_status(ii, pr)
+    local sum = 0
+    for k,v in pairs(pr) do
+        sum = sum + v
+    end
+    io.write('iter#',ii,' ', sum, ' ')
+    for k,v in pairs(pr) do
+        io.write(string.format('%s -> %f ', k, v))
+    end
+    io.write('\n')
+end
+
+function pagerank(adj, max_iter, damp)
+    local pr = {}
+    for uu in adj:vertices() do
+        pr[uu] = 1/adj.num_elems
+    end
+    local iter = 0
+    while iter < max_iter do
+        local newpr = {}
+        local change = 0
+        for uu in adj:vertices() do
+            local sum = 0
+            for vv in adj:incoming(uu) do
+                sum = sum + pr[vv]/adj.num_outgoing[vv]
+            end
+            newpr[uu] = (1-damp)/adj.num_elems + damp*sum
+            change = change + math.abs(pr[uu] - newpr[uu])
+        end
+        pr = newpr
+        if change < 1e-3 then
+            break
+        end
+        iter = iter+1
+    end
+    pagerank_status(0, pr)
+    return pr
+end
+
+function pagerank_root(root, adj, max_iter, damp, alpha)
+    local pr = {}
+    for uu in adj:vertices() do
+        pr[uu] = 1/adj.num_elems
+    end
+    local iter = 0
+    while iter < max_iter do
+        local newpr = {}
+        local change = 0
+        for uu in adj:vertices() do
+            local sum = 0
+            for vv in adj:incoming(uu) do
+                sum = sum + pr[vv]/adj.num_outgoing[vv]
+            end
+            newpr[uu] = (1-damp)/adj.num_elems + (1-alpha)*damp*sum + alpha*pr[root]
+            change = change + math.abs(pr[uu] - newpr[uu])
+        end
+        pr = newpr
+        if change < 1e-3 then
+            break
+        end
+        iter = iter+1
+    end
+    pagerank_status(iter, pr)
+    return pr
+end
+
+function main()
+    --local adj = load_train('data/noisy-blocks.gml')
+    local adj = load_train('data/ila/nips/nips_1_train.mat_train.gml')
+    adj:print()
+    local pr = pagerank(adj, 100, 0.85, 0.1)
+    for root in adj:vertices() do
+        print('root',root)
+        pagerank_root(root, adj, 100, 0.85, 0.1)
+    end
+end
+
+main()
