3aa6cde33bf1bb4ea69221af321f7e88481acb85
charles blundell <c.blundell@gatsby.ucl.ac.uk>
2013-05-31 17:34:24 +0100

    revert a few recent changes to known good.
    symmetry makes things very slow (and isn't technically wrong).
    
    offscore is unnecessary.

--- src/dataset_gml.c
+++ src/dataset_gml.c
@@ -14,7 +14,7 @@ Dataset * dataset_gml_load(const gchar *fname) {
 	gchar *next;
 
 	id_labels = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
-	dd = dataset_new(TRUE);
+	dd = dataset_new(FALSE);
 	dataset_set_filename(dd, fname);
 	toks = tokens_open(fname);
 	while (tokens_has_next(toks)) {
