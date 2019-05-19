30f8ee80be56fbddc96cbdd393b78dc0eb97f63b
zhaofeng-shu33 <616545598@qq.com>
2019-05-19 18:47:10 +0800

    add cmake include

--- src/bhcd/version.h
+++ src/bhcd/version.h
@@ -3,7 +3,7 @@
 
 #include "git_version.h"
 
-#define BHCD_VERSION VERSION"#"GIT_VERSION
+#define BHCD_VERSION VERSION"#"GIT_VERSION 
 #define BHCD_NAME_VERSION   PACKAGE" "BHCD_VERSION
 
 #endif /*VERSION_H*/
