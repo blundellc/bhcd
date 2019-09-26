4697a840d11d9d467aac532b4919f24646e3869d
zhaofeng-shu33 <616545598@qq.com>
2019-09-26 21:19:12 +0800

    add more definitions

--- pybhcd.pyx
+++ pybhcd.pyx
@@ -1,7 +1,23 @@
 # distutils: language = c
 cdef extern from "bhcd/bhcd/bhcd.h":
     ctypedef struct GRand
+    ctypedef struct Tree
+    ctypedef struct Dataset
+    ctypedef struct Build
+    ctypedef struct Params
+    ctypedef char gchar
+    ctypedef unsigned int guint
+    ctypedef int gint
+    ctypedef gint gboolean
     GRand* g_rand_new()
-
-cdef bhcd():
-    g_rand_new()
+    void tree_io_save_string(Tree*, gchar**)
+    Build * build_new(GRand *rng, Params * params, guint num_restarts, gboolean sparse)
+    Dataset * dataset_gml_load(const gchar*)
+    
+cpdef bhcd(gml_py_str):
+    cdef int a = 1
+    cdef double b = <double> a
+    cdef char* gml_c_str = gml_py_str
+    rng_ptr = g_rand_new()
+    dataset_ptr = dataset_gml_load( <gchar*> gml_c_str)
+    
